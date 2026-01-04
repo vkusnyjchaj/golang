8fb41137b3d307aa0b619bf14e0d6a407a21206a
vkusnyjchaj <vkusnyjchaj@gmail.com>
2026-01-04 12:55:16 +0700

    Add exercise errors

--- Tour_of_go/Exercise_errors/errors.go
+++ Tour_of_go/Exercise_errors/errors.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %d", float64(e))
+}
+
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	return math.Sqrt(x), nil
 }
 
 func main() {
