faccac1954ea6438bdda5f1cd3c99a79bdcc4587
vkusnyjchaj <vkusnyjchaj@gmail.com>
2026-01-04 14:28:11 +0700

    Add exercise image

---
+++ Tour_of_go/Exercise_images/images.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"image"
+	"image/color"
+	"image/png"
+	"io"
+	"os"
+)
+
+// ShowImage displays the image m
+// when executed on the Go Playground.
+func ShowImage(m image.Image) {
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	io.WriteString(w, "IMAGE:")
+	b64 := base64.NewEncoder(base64.StdEncoding, w)
+	err := (&png.Encoder{CompressionLevel: png.BestCompression}).Encode(b64, m)
+	if err != nil {
+		panic(err)
+	}
+	b64.Close()
+	io.WriteString(w, "\n")
+}
+
+type Image struct {
+	w, h int
+}
+
+func (img Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.w, img.h)
+}
+
+func (img Image) At(x, y int) color.Color {
+	v := uint8((x + y) / 2)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{200, 200}
+	ShowImage(m)
+}
